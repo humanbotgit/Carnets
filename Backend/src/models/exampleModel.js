96fc34d956c4eb7843adb0de8be1c0261d2a6897
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-27 14:51:01 -0500

    .env created and related

--- Backend/src/models/exampleModel.js
+++ Backend/src/models/exampleModel.js
@@ -2,7 +2,7 @@ const db = require('../../config/db');
 
 const getExampleData = async () => {
     try {
-        const [rows] = await db.query('SELECT * FROM carnet');
+        const [rows] = await db.query('SELECT * FROM ambiente');
         return rows;
     } catch (err) {
         throw new Error('Error fetching data: ' + err.message);
