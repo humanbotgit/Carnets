ce2df7ac4537eb2d1a91f0a0febcd3f32d5c9e4a
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-27 15:03:27 -0500

    campusController

---
+++ Backend/src/controllers/campusController.js
@@ -0,0 +1,19 @@
+const campusModel = require('../models/campusModel');
+
+exports.getCampus = async (req, res) => {
+    try {
+        const data = await campusModel.getCampus();
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+};
+exports.postCampus = async (req, res) => {
+    try {
+        const campus = req.params.campus
+        const data = await campusModel.postCampus(campus);
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to post data' });
+    }
+};
