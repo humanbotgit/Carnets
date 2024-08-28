90ea5af6a8e70a1dbbb796e79e007cbed4a8d4bd
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-28 10:09:24 -0500

    postPabellon creado

--- Backend/src/models/ambientesSoftware.Model.js
+++ Backend/src/models/ambientesSoftware.Model.js
@@ -7,6 +7,16 @@ const getPabellones = async (campus_id) =>{
         throw new Error('Error fetching data: ' + err.message);
     }
 }
+const postPabellon = async (values) => {
+    try {
+        const [rows] = await db.query(
+            'INSERT INTO campus(campus_id,pabellon_nombre) VALUES (?,?)',values.campus_id,values.pabellon_nombre);
+        return [rows]
+    } catch (error) {
+        throw new Error('Error posting data: ' + err.message);
+    }
+}
 module.exports = {
-    getPabellones
+    getPabellones,
+    postPabellon
 }
