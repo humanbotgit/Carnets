9f0caec56859e0ff1b96c922a759510212a0e70e
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-28 09:59:15 -0500

    getPabellones controller

--- Backend/src/controllers/ambientesSoftwareController.js
+++ Backend/src/controllers/ambientesSoftwareController.js
@@ -1,19 +1,10 @@
 const ambientesSoftware = require('../models/ambientesSoftware.Model');
 
-exports.getCampus = async (req, res) => {
+exports.getPabellones = async (req, res) => {
     try {
-        const data = await campusModel.getCampus();
+        const data = await ambientesSoftware.getPabellones();
         res.json(data);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
-    }
-};
-exports.postCampus = async (req, res) => {
-    try {
-        const campus = req.body.campus
-        const data = await campusModel.postCampus(campus);
-        res.json(data);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to post data' });
     }
 };
