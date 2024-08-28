68bc72201133eb361fbbf73db3717ac3e4ee405e
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-28 10:13:10 -0500

    ruta postPabellon

--- Backend/src/routes/ambientesSoftwareRoutes.js
+++ Backend/src/routes/ambientesSoftwareRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ambientesSoftwareController = require('../controllers/ambientesSoftwareController');
 
 router.get('/pabellon/:campus_id',ambientesSoftwareController.getPabellones)
+router.post('pabellon',ambientesSoftwareController.postPabellon)
 
 module.exports = router
