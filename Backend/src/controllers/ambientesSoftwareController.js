98a0cfb7a9a23fd30f5bf21972f328a1d4582dec
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-28 10:06:05 -0500

    corrigiendo el param con params

--- Backend/src/controllers/ambientesSoftwareController.js
+++ Backend/src/controllers/ambientesSoftwareController.js
@@ -2,7 +2,7 @@ const ambientesSoftware = require('../models/ambientesSoftware.Model');
 
 exports.getPabellones = async (req, res) => {
     try {
-        const campus_id = req.param.campus_id
+        const campus_id = req.params.campus_id
         const data = await ambientesSoftware.getPabellones(campus_id);
         res.json(data);
     } catch (err) {
