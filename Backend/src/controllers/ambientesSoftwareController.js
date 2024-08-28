bdd1daec18502058180bbd7c88fed61351a03107
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-08-28 10:12:06 -0500

    postPabellon ambientesSoftware

--- Backend/src/controllers/ambientesSoftwareController.js
+++ Backend/src/controllers/ambientesSoftwareController.js
@@ -9,3 +9,12 @@ exports.getPabellones = async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 };
+exports.postPabellon = async (req, res) => {
+    try {
+        const values = req.body.values
+        const data = await ambientesSoftware.postPabellon(values);
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to post data' });
+    }
+};
